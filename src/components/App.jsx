7a6e29273d6b8853328be41cbbbd68ebe39baa50
Dafarlog <Dafarlog@gmail.com>
2022-09-30 16:37:17 +0300

    add propsTypes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,15 @@
-import Profile from "./Profile";
+import Profile from "./profile/Profile";
 import user from "../user.json"
 export const App = () => {
+  const {username, tag, location, avatar, stats:{followers, views, likes}} = user;
   return (
-    <Profile username = {user.username}
-    tag = {user.tag}
-    avatar = {user.avatar}
-    location = {user.location}
-    stats = {user.stats}/>
+    <Profile 
+    username = {username}
+    tag = {tag}
+    avatar = {avatar}
+    location = {location}
+    followers = {followers}
+    views = {views}
+    likes = {likes}/>
   );
 };
--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,9 +1,9 @@
-export default function Profile({username, tag, location, avatar, stats:{followers, views, likes}}){
+import PropTypes from 'prop-types';
+export default function Profile({username, tag, location, avatar, followers, views, likes}){
    return (
     <div className="profile">
     <div className="description">
         <img
-        // src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
         src={avatar||"https://cdn-icons-png.flaticon.com/512/1077/1077012.png"}
         alt="User avatar"
         className="avatar"
@@ -28,6 +28,15 @@ export default function Profile({username, tag, location, avatar, stats:{followe
         </li>
         </ul>
     </div>
-   )
-   
+   )  
+}
+
+Profile.protoTypes ={
+    username: PropTypes.string.isRequired, 
+    tag: PropTypes.string.isRequired, 
+    location: PropTypes.string.isRequired, 
+    avatar: PropTypes.string.isRequired, 
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired, 
+    likes: PropTypes.number.isRequired,
 }
