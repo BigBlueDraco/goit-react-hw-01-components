5e37ed8ccfcfd45b974743ce501d0583f5cfc9cd
Dafarlog <Dafarlog@gmail.com>
2022-10-03 11:48:10 +0300

    add friend list item css

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,10 +1,11 @@
 import PropTypes from 'prop-types';
+import css from "./friend-list-item.module.css"
 export default function FriendListItem({avatar, name, isOnline = false}){
     return(
-    <li className="item">
-        {isOnline? <span className = "online">online</span> : <span className = "offline">offline</span>}
-        <img className="avatar" src={avatar} alt="User avatar" width="48" />
-        <p className="name">{name}</p>
+    <li className={css.item}>
+        {isOnline? <span className = {`${css.online} ${css.avatarFrame}`}><img className="avatar" src={avatar} alt="User avatar" width="48" /></span>
+        : <span className = {`${css.offline} ${css.avatarFrame}`}><img className="avatar" src={avatar} alt="User avatar" width="48" /></span>}
+        <p className={css.name}>{name}</p>
     </li>
     );
 }
